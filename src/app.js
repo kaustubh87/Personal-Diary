3b939a5ec3b578aa4acb76fa2044af8ff5d93c08
kvin1087 <kaustubh87@gmail.com>
2017-11-13 16:34:39 -0600

    Added mongoose connection and Routes

--- src/app.js
+++ src/app.js
@@ -6,34 +6,25 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var appRoutes = require('./server/routes/app');
+
 var app = express();
-mongoose.connect('localhost:27017/personal-diary');
+mongoose.connect('localhost:27017/diary');
+
+// view engine setup
+app.set('views', path.join(__dirname, './server/views'));
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', function (req, res) {
-res.sendFile(path.join(__dirname,'index.html'));
-});
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-var err = new Error('Not Found');
-err.status = 404;
-next(err);
-});
-var env = 'development';
-if (app.get(env == 'development'))
-{
-app.listen(3000, function () {
-console.log('Example listening on port 3000');
-});
-}
-else{
+app.use('/', appRoutes);
+
 app.listen(8080, function () {
 console.log('Example listening on port 8080');
 });
-}
+
 module.exports = app;
