3caa4b81d6c824f7317f90ab75e42a8aac946594
kvin1087 <kaustubh87@gmail.com>
2017-11-12 20:06:46 -0600

    Added database connection

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
 var app = express();
 mongoose.connect('localhost:27017/personal-diary');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
