0aa98d46cd6854d059e8d96afcc0df253831c98b
kvin1087 <kaustubh87@gmail.com>
2017-11-13 21:31:08 -0600

    Added post method to save user data

--- src/server/routes/app.js
+++ src/server/routes/app.js
@@ -10,5 +10,22 @@ router.get('/', function (req, res, next) {
     res.render('node');
 });
 
+router.post('/save', function (req, res , next){
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var password = req.body.password;
+    var email = req.body.email;
+
+    var user = new User({
+      firstName: firstname,
+      lastName: lastname,
+      password: password,
+      email: email
+    });
+
+    user.save();
+    res.redirect('/');
+});
+
 
 module.exports = router;
